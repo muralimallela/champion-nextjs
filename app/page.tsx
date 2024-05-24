8e40235094c20e9037a3172cf7ed38bf896a5355
Murali Krishna Mallela <mmallela9@gmail.com>
2024-05-24 22:09:26 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import TeamSection from "@/components/Team/team";
 import ServiceSection from "@/components/Services/serviceSection";
 import TestimonialsSection from "@/components/Testimonials/testimonialsSection";
 import AboutSection from "@/components/About/aboutSection";
-import Hero from "@/components/Hero";
 import { Divider } from 'primereact/divider';
 export default function Home() {
   return (
